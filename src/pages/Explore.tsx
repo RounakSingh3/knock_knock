b8a48308ff93d35e0d29a6b1bc90ca7ba33eaf35
RounakSingh3 <RounakSingh3@users.noreply.github.com>
2026-08-05 16:11:26 +0530

    Implement Instant Bollywood viral entertainment hub with celebrity spotted cards, movie updates, and song reels

--- src/pages/Explore.tsx
+++ src/pages/Explore.tsx
@@ -13,6 +13,7 @@ import ChatPanel from '../components/ChatPanel';
 import PullToRefresh from '../components/PullToRefresh';
 import { isVideoPost, isVideoUrl } from '../lib/media';
 import { GridSkeleton, TrendingSkeleton } from '../components/SkeletonLoader';
+import { BollywoodSection } from '../components/BollywoodSection';
 
 function groupByUser(stories: StoryData[]): UserStoryGroup[] {
     const groups: Record<string, UserStoryGroup> = {};
@@ -360,6 +361,9 @@ const Explore = () => {
                                 </div>
                             )}
                             {isTrendingLoading && <TrendingSkeleton />}
+
+                            {/* 🎬 Instant Bollywood Viral Entertainment Hub */}
+                            <BollywoodSection onSelectPost={(post) => setActiveFeedState({ posts: [post], index: 0 })} />
 
                             {/* Discover Grid */}
                             {isDiscoverLoading ? (
